c7c0832031855268b999417693241354eac27adf
Harsh <harsh.stephen189@gmail.com>
2025-11-06 14:39:47 +0530

    finnal final for depolment

--- duoko/settings.py
+++ duoko/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-23)_^w7l(=00(zu@37=m)kv1n=^!0bgb=$mrg!jiq8)6bw25!5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
